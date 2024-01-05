cd9693fa18bb7b25f78c0f00a6227a08ae527062
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-06 02:14:33 +0900

    Minimize structure

--- src/Relic/Add/RelicAddMain.jsx
+++ src/Relic/Add/RelicAddMain.jsx
@@ -3,80 +3,79 @@ import "./RelicAddMain.css";
 
 export default class RelicAddMain extends Component {
   render() {
-    let mainOptList = [<option key="-">-</option>];
-    let mainOptSet = [];
+    let mainOptList = [];
     switch (this.props.slot) {
       case "머리":
-        mainOptList = <option key="HP">HP</option>;
+        mainOptList = ["HP"];
         break;
       case "팔":
-        mainOptList = <option key="공격력">공격력</option>;
+        mainOptList = ["공격력"];
+        break;
+      case "몸통":
+        mainOptList = [
+          "HP%",
+          "공격력%",
+          "방어력%",
+          "치명타 확률",
+          "치명타 피해",
+          "효과 명중",
+          "치유량 보너스",
+        ];
+        break;
+      case "다리":
+        mainOptList = ["HP%", "공격력%", "방어력%", "속도"];
+        break;
+      case "구체":
+        mainOptList = [
+          "HP%",
+          "공격력%",
+          "방어력%",
+          "물리 속성 피해 증가",
+          "화염 속성 피해 증가",
+          "얼음 속성 피해 증가",
+          "번개 속성 피해 증가",
+          "바람 속성 피해 증가",
+          "양자 속성 피해 증가",
+          "허수 속성 피해 증가",
+        ];
+        break;
+      case "매듭":
+        mainOptList = [
+          "HP%",
+          "공격력%",
+          "방어력%",
+          "에너지 회복효율",
+          "격파 특수효과",
+        ];
         break;
       default:
-        switch (this.props.slot) {
-          case "몸통":
-            mainOptSet = [
-              "HP%",
-              "공격력%",
-              "방어력%",
-              "치명타 확률",
-              "치명타 피해",
-              "치유량 보너스",
-              "효과 명중",
-            ];
-            break;
-          case "다리":
-            mainOptSet = ["HP%", "공격력%", "방어력%", "속도"];
-            break;
-          case "구체":
-            mainOptSet = [
-              "HP%",
-              "공격력%",
-              "방어력%",
-              "물리 속성 피해 증가",
-              "화염 속성 피해 증가",
-              "얼음 속성 피해 증가",
-              "번개 속성 피해 증가",
-              "바람 속성 피해 증가",
-              "양자 속성 피해 증가",
-              "허수 속성 피해 증가",
-            ];
-            break;
-          default:
-            mainOptSet = [
-              "HP%",
-              "공격력%",
-              "방어력%",
-              "에너지 회복효율",
-              "격파 특수효과",
-            ];
-            break;
-        }
-        for (const mainOpt of mainOptSet) {
-          mainOptList.push(
-            <option key={mainOpt} value={mainOpt}>
-              {mainOpt}
-            </option>
-          );
-        }
         break;
+    }
+    let innerOpt = [];
+    for (const mainOpt of mainOptList) {
+      innerOpt.push(
+        <option key={mainOpt} value={mainOpt}>
+          {mainOpt}
+        </option>
+      );
     }
     return (
       <section className="RelicAddMain">
         <div className="RelicAddMainKey">
           <h3>주 옵션</h3>
           <select
+            required
             name="main"
             id=""
             onChange={function (e) {
               this.props.onMainOpt(e.target.value);
             }.bind(this)}
           >
-            {mainOptList}
+            {innerOpt}
           </select>
         </div>
         <div className="RelicAddLevel">
-          <h3>Level</h3>
+          <h3>레벨</h3>
           <input
             type="number"
             name="level"
