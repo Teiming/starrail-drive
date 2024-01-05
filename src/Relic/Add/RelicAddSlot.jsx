a1abdd7e5d4f3480edbb29ff204dfb287a4e74d3
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-06 02:03:54 +0900

    Adjust structure

--- src/Relic/Add/RelicAddSlot.jsx
+++ src/Relic/Add/RelicAddSlot.jsx
@@ -3,31 +3,51 @@ import "./RelicAddSlot.css";
 
 export default class RelicAddSlot extends Component {
   render() {
-    const slotSet = ["머리", "팔", "몸통", "다리", "구체", "매듭"];
-    let result = [];
-    for (const slot of slotSet) {
-      result.push(
-        <input
-          key={slot}
-          type="radio"
-          name="slot"
-          value={slot}
-          id={slot}
-          onChange={function (e) {
-            this.props.onChecked(e.target.value);
-          }.bind(this)}
-        />
-      );
-      result.push(
+    const slotSetA = ["머리", "팔", "몸통", "다리"];
+    const slotSetB = ["구체", "매듭"];
+    let resultA = [];
+    let resultB = [];
+    for (const slot of slotSetA) {
+      resultA.push(
         <label key={slot + " label"} htmlFor={slot}>
           {slot}
+          <input
+            key={slot}
+            type="radio"
+            name="slot"
+            value={slot}
+            id={slot}
+            onChange={function (e) {
+              this.props.onChecked(e.target.value);
+            }.bind(this)}
+          />
+        </label>
+      );
+    }
+    for (const slot of slotSetB) {
+      resultB.push(
+        <label key={slot + " label"} htmlFor={slot}>
+          {slot}
+          <input
+            key={slot}
+            type="radio"
+            name="slot"
+            value={slot}
+            id={slot}
+            onChange={function (e) {
+              this.props.onChecked(e.target.value);
+            }.bind(this)}
+          />
         </label>
       );
     }
     return (
       <section className="RelicAddSlot">
         <h3>부위</h3>
-        <div className="radio">{result}</div>
+        <div className="RelicAddSlotContainer">
+          <div className="RelicAddSlotRadio">{resultA}</div>
+          <div className="RelicAddSlotRadio">{resultB}</div>
+        </div>
       </section>
     );
   }
