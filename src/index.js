451b9d521c682a29520599ee08b88e15a03cb1ac
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-19 15:07:33 +0900

    Allow single quotes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import StarrailDrive from "StarrailDrive";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import StarrailDrive from 'StarrailDrive';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StarrailDrive />
