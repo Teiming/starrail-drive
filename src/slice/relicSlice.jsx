1e85ccfaa075f977ec8a07ead2fe3a4d80d9fe0a
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-19 14:59:48 +0900

    Create slice

--- src/slice/relicSlice.jsx
+++ src/slice/relicSlice.jsx
@@ -2,27 +2,51 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const name = "relicSlice";
 
-let initialState = {};
+let initialState = { relic: {} };
 
-let storedRelic = localStorage.getItem("유물");
+const storedRelic = localStorage.getItem("유물");
 if (storedRelic) {
-  initialState = JSON.parse(storedRelic);
+  initialState = { relic: JSON.parse(storedRelic) };
 }
+
+const saveRelic = (relic) => {
+  localStorage.setItem("유물", JSON.stringify(relic));
+};
 
 const reducers = {
   addRelic: (state, action) => {
-    state[action.payload.id] = action.payload.relicData;
+    state.relic[action.payload.id] = action.payload.relicData;
+    saveRelic(state.relic);
+  },
+  updateRelicLevel: (state, action) => {
+    state.relic[action.payload.relicID]["레벨"] = action.payload.level;
+    state.relic[action.payload.relicID]["부옵션"] = action.payload.sub;
+    saveRelic(state.relic);
   },
   updateRelic: (state, action) => {
-    state[action.payload.relicID]["레벨"] = action.payload.level;
-    state[action.payload.relicID]["부옵션"] = action.payload.sub;
+    state.relic[action.payload.relicID]["레벨"] = action.payload.level;
+    state.relic[action.payload.relicID]["부옵션"] = action.payload.sub;
+    saveRelic(state.relic);
   },
-  saveRelic: (state) => {
-    localStorage.setItem("유물", JSON.stringify(state.유물));
+  updateRelicEquip: (state, action) => {
+    state.relic[action.payload.id]["장착"] = action.payload.newEquip;
+    saveRelic(state.relic);
+  },
+  deleteRelic: (state, action) => {
+    let _relic = Object.assign({}, state.relic);
+    delete _relic[action.payload];
+    state.relic = _relic;
+    saveRelic(state.relic);
   },
 };
 
 const relicSlice = createSlice({ name, initialState, reducers });
 
-export const { addRelic, updateRelic, saveRelic } = relicSlice.actions;
+export const {
+  addRelic,
+  updateRelicLevel,
+  updateRelic,
+  updateRelicEquip,
+  deleteRelic,
+} = relicSlice.actions;
 export default relicSlice.reducer;
