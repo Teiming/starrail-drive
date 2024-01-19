f61c16f2c41362e1a810b9a6ff75ef718b633540
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-19 15:01:53 +0900

    New Features 	- Delete relic

--- src/Relic/RelicList.jsx
+++ src/Relic/RelicList.jsx
@@ -2,15 +2,34 @@ import { Component } from "react";
 import RelicCard from "./Card/RelicCard";
 import store from "store";
 import { subMode } from "slice/modeSlice";
+import { deleteRelic, updateRelicEquip } from "slice/relicSlice";
 import "./RelicList.css";
 
 export default class RelicList extends Component {
   state = {
     filter: store.getState().filterSlice.relic,
+    relicData: store.getState().relicSlice.relic,
   };
   render() {
+    const relicData = this.state.relicData;
+    let innerList = [];
+    for (const id in relicData) {
+      innerList.push(
+        <RelicCard
+          key={id}
+          isSelected={this.state.filter[relicData[id]["부위"]]}
+          relicData={relicData[id]}
+          onDelete={() => {
+            store.dispatch(deleteRelic(id));
+          }}
+          onChangeEquip={(newEquip) => {
+            store.dispatch(updateRelicEquip({ id, newEquip }));
+          }}
+        />
+      );
+    }
     return (
-      <div className="RelicList">
+      <article className="RelicList">
         <div
           className="controler"
           onClick={() => {
@@ -19,98 +38,20 @@ export default class RelicList extends Component {
         >
           <span>+</span>
         </div>
-        <RelicCard
-          set="dfsf"
-          slot="다리"
-          level="15"
-          equip="단항_음월"
-          main="공격력"
-          sub1="dfsf"
-          sub2="dfsf"
-          sub3="dfsfdfdf"
-          sub4="2123"
-          sub1_value="0.21"
-          sub2_value="0.22"
-          sub3_value="0.23"
-          sub4_value="0.24"
-          isSelected={this.state.filter["다리"]}
-        />
-        <RelicCard
-          set="들이삭과 동행하는 거너"
-          slot="다리"
-          equip="정운"
-          main="공격력"
-          sub1="dfsf"
-          sub2="dfsf"
-          sub3="dfsfdfdf"
-          sub4="2123"
-          sub4_value="0.23"
-          isSelected={this.state.filter["다리"]}
-        />
-        <RelicCard
-          set="들이삭과 동행하는 거너"
-          slot="다리"
-          equip="정운"
-          main="공격력"
-          sub1="dfsf"
-          sub2="dfsf"
-          sub3="dfsfdfdf"
-          sub4="2123"
-          sub4_value="0.23"
-          isSelected={this.state.filter["다리"]}
-        />
-        <RelicCard
-          set="들이삭과 동행하는 거너"
-          slot="다리"
-          equip="정운"
-          main="공격력"
-          sub1="dfsf"
-          sub2="dfsf"
-          sub3="dfsfdfdf"
-          sub4="2123"
-          sub4_value="0.23"
-          isSelected={this.state.filter["다리"]}
-        />
-        <RelicCard
-          set="dfsf"
-          slot="다리"
-          equip="부현"
-          main="HP"
-          sub1="dfsf"
-          sub2="dfsf"
-          sub3="dfsfdfdf"
-          sub4="2123"
-          isSelected={this.state.filter["다리"]}
-        />
-        <RelicCard
-          set="dfsf"
-          slot="머리"
-          equip="정운"
-          main="HP"
-          sub1="dfsf"
-          sub2="dfsf"
-          sub3="dfsfdfdf"
-          sub4="2123"
-          isSelected={this.state.filter["머리"]}
-        />
-        <RelicCard
-          set="dfsf"
-          slot="몸통"
-          equip="정운"
-          main="HP"
-          sub1="dfsf"
-          sub2="dfsf"
-          sub3="dfsfdfdf"
-          sub4="2123"
-          isSelected={this.state.filter["몸통"]}
-        />
-      </div>
+        {innerList}
+      </article>
     );
   }
   componentDidMount() {
     store.subscribe(
       function () {
         this.setState({ filter: store.getState().filterSlice.relic });
+      }.bind(this)
+    );
+    store.subscribe(
+      function () {
+        const relicData = Object.assign({}, store.getState().relicSlice.relic);
+        this.setState({ relicData });
       }.bind(this)
     );
   }
