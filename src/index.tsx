20f4659457b232cce9a8b811540d5a90b0e82631
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-21 23:56:25 +0900

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import StarrailDrive from 'StarrailDrive';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <StarrailDrive />
-  </React.StrictMode>
-);
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <StrictMode>
+      <StarrailDrive />
+    </StrictMode>
+  );
+}
