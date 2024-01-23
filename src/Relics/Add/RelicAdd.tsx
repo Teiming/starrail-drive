1bee74a34ce76802bee4a369a75e7d146cb53c50
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-24 06:13:36 +0900

    design

--- src/Relics/Add/RelicAdd.tsx
+++ src/Relics/Add/RelicAdd.tsx
@@ -4,15 +4,16 @@ import RelicAddSet from './RelicAddSet';
 import RelicAddMain from './RelicAddMain';
 import RelicAddSub from './RelicAddSub';
 import RelicAddEquip from './RelicAddEquip';
+import Empty from 'Empty';
 import { dispatch } from 'store';
 import { switchSubMode } from 'slice/modeSlice';
 import { addRelics } from 'slice/relicsSlice';
-import { EveryRelicSlot } from 'types/every';
+import { EveryRelicsSlot } from 'types/every';
 import { RelicSubOption } from 'types/relics';
 import './RelicAdd.css';
 
 export default function RelicAdd() {
-  const [slot, setSlot] = useState<EveryRelicSlot>('머리');
+  const [slot, setSlot] = useState<EveryRelicsSlot>('머리');
   const [level, setLevel] = useState(0);
   const [main, setMain] = useState('');
   const [sub, setSub] = useState<RelicSubOption>({});
@@ -23,67 +24,70 @@ export default function RelicAdd() {
   const newID = genID() + '-' + genID();
 
   return (
-    <form
-      className='RelicAdd'
-      onSubmit={(e) => {
-        e.preventDefault();
-        const target = e.target as HTMLFormElement;
-        dispatch(
-          addRelics({
-            id: '유물_' + newID,
-            relicData: {
-              세트: target.set.value,
-              부위: slot,
-              주옵션: target.main.value,
-              레벨: level,
-              부옵션: sub,
-              장착: equip,
-            },
-          })
-        );
-        dispatch(switchSubMode(''));
-      }}
-    >
-      <input type='hidden' name='relicId' value={'유물_' + newID} />
-      <RelicAddSlot
-        onChecked={(slot: EveryRelicSlot) => {
-          setSlot(slot);
-          if (slot === '머리') {
-            setMain('HP');
-          } else if (slot === '팔') {
-            setMain('공격력');
-          } else {
-            setMain('HP%');
-          }
+    <main id='RelicsAdd'>
+      <form
+        id='RelicsAddForm'
+        onSubmit={(e) => {
+          e.preventDefault();
+          const target = e.target as HTMLFormElement;
+          dispatch(
+            addRelics({
+              id: '유물_' + newID,
+              relicData: {
+                세트: target.set.value,
+                부위: slot,
+                주옵션: target.main.value,
+                레벨: level,
+                부옵션: sub,
+                장착: equip,
+              },
+            })
+          );
+          dispatch(switchSubMode(''));
         }}
-      />
-      <RelicAddSet slot={slot} />
-      <RelicAddMain
-        slot={slot}
-        level={level}
-        onMain={(main: string) => {
-          setMain(main);
-        }}
-        onLevel={(level: number) => {
-          setLevel(level);
-          setMaxLine(1 + Math.floor(level / 3));
-        }}
-      />
-      <RelicAddSub
-        main={main}
-        maxLine={maxLine}
-        onSub={() => {
-          setSub({});
-        }}
-      />
-      <RelicAddEquip
-        onEquip={(name: string) => {
-          setEquip(name);
-        }}
-      />
-      <section className='RelicAddSubmit'>
-        <input type='submit' value='추가' />
-      </section>
-    </form>
+      >
+        <input type='hidden' name='relicId' value={'유물_' + newID} />
+        <RelicAddSlot
+          onChecked={(slot: EveryRelicsSlot) => {
+            setSlot(slot);
+            if (slot === '머리') {
+              setMain('HP');
+            } else if (slot === '팔') {
+              setMain('공격력');
+            } else {
+              setMain('HP%');
+            }
+          }}
+        />
+        <RelicAddSet slot={slot} />
+        <RelicAddMain
+          slot={slot}
+          level={level}
+          onMain={(main: string) => {
+            setMain(main);
+          }}
+          onLevel={(level: number) => {
+            setLevel(level);
+            setMaxLine(1 + Math.floor(level / 3));
+          }}
+        />
+        <RelicAddSub
+          main={main}
+          maxLine={maxLine}
+          onSub={() => {
+            setSub({});
+          }}
+        />
+        <RelicAddEquip
+          onEquip={(name: string) => {
+            setEquip(name);
+          }}
+        />
+        <section className='RelicAddSubmit'>
+          <input type='submit' value='추가' />
+        </section>
+      </form>
+      <Empty line={1} />
+    </main>
   );
 }
