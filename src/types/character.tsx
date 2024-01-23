98a7f024693a1eb59c2b8be9f841df5ba7ac9e12
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-24 05:34:15 +0900

    feat

--- src/types/character.tsx
+++ src/types/character.tsx
@@ -6,12 +6,17 @@ export interface Characters {
   개척자: CharacterTrailblazer;
 }
 
+export type TraceMajor = [primary: true | false, secondary: true | false, tertiary: true | false];
+export type TraceMinor = Array<true | false>;
+
 export type TraceLevel = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 export interface Trace {
   일반공격: Exclude<TraceLevel, 7 | 8 | 9 | 10>;
   전투스킬: TraceLevel;
   필살기: TraceLevel;
   특성: TraceLevel;
+  능력: TraceMajor;
+  속성: TraceMinor;
 }
 export type Eidolon = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 export type TraitLevelKeys = keyof Trace;
