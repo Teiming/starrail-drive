4fa5a5c50f29d3fca4a001451927cb1539d99bf6
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-24 13:27:02 +0900

    feat

--- src/Relics/Card/RelicCardBody.tsx
+++ src/Relics/Card/RelicCardBody.tsx
@@ -14,15 +14,60 @@ export default function RelicCardBody(props: Props) {
       const _number = 100 * Number(params);
       return _number + '%';
     } else {
-      return params;
+      return params.toString();
     }
   };
-  let mainValue;
+  const getMainValue = (zero: number) => {
+    return (level: number) => {
+      return (zero * 100000 * (1 + (35 * level) / 100)) / 100000;
+    };
+  };
+  const coeffi = {
+    HP: 112.9,
+    HP_: 0.06912,
+    speed: 4,
+    critDMG: 0.10368,
+    heal: 0.0553,
+    ele: 0.06222,
+  } as const;
+  let _getMainValue;
+  switch (props.main) {
+    case 'HP':
+      _getMainValue = getMainValue(coeffi.HP);
+      break;
+    case '공격력':
+      _getMainValue = getMainValue(0.5 * coeffi.HP);
+      break;
+    case 'HP%':
+    case '공격력%':
+    case '효과 명중':
+      _getMainValue = getMainValue(coeffi.HP_);
+      break;
+    case '방어력%':
+      _getMainValue = getMainValue(1.25 * coeffi.HP_);
+      break;
+    case '치명타 확률':
+      _getMainValue = getMainValue(0.5 * coeffi.critDMG);
+      break;
+    case '치명타 피해':
+    case '격파 특수효과':
+      _getMainValue = getMainValue(coeffi.critDMG);
+      break;
+    case '치유량 보너스':
+      _getMainValue = getMainValue(coeffi.heal);
+      break;
+    case '에너지 회복효율':
+      _getMainValue = getMainValue(0.5 * coeffi.ele);
+      break;
+    default:
+      _getMainValue = getMainValue(coeffi.ele);
+      break;
+  }
   return (
     <section className='RelicCardBody'>
       <div className='RelicMain'>
         <span>{props.main}</span>
-        <span>{mainValue}</span>
+        <span>{makePercent(_getMainValue(props.level))}</span>
       </div>
       <dl className='RelicSub'>
         <dt>wndhq</dt>
