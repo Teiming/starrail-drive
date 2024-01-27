9db968663bdb29b62a72efa67a5d3446063f9100
Teiming <36464297+Teiming@users.noreply.github.com>
2024-01-27 20:39:14 +0900

    feat

--- src/slice/grobalSlice.tsx
+++ src/slice/grobalSlice.tsx
@@ -5,7 +5,7 @@ const name = 'grobalSlice';
 
 let initialState: { page: EveryPage; mode: EveryMode } = { page: '캐릭터', mode: '' };
 
-const storedMode = sessionStorage.getItem('mode');
+const storedMode = sessionStorage.getItem('page');
 if (storedMode) {
   function modeCheck(value: string): value is EveryPage {
     return everyPage.includes(value as EveryPage);
--- src/types/mode.tsx
+++ src/types/mode.tsx
@@ -1,4 +1,4 @@
-export const everyPage = ['캐릭터', '광추', '유물'] as const;
+export const everyPage = ['캐릭터', '광추', '유물', '설정'] as const;
 export type EveryPage = (typeof everyPage)[number];
 
 export const everyMode = ['', '추가', '수정', '상세'] as const;
